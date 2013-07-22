3bbe6376e66c3654f68d2c7cb7ff614cc0597b78
Benjamin Goering <bengoering@gmail.com>
2013-07-22 16:34:37 -0700

    init

---
+++ requirejs.conf.js
@@ -0,0 +1,35 @@
+require.config({
+  baseUrl: '/',
+  paths: {
+    jquery: 'lib/jquery/jquery',
+    text: 'lib/requirejs-text/text',
+    base64: 'lib/base64/base64',
+    hogan: 'lib/hogan/web/builds/2.0.0/hogan-2.0.0.amd',
+    hgn: 'lib/requirejs-hogan-plugin/hgn',
+    jasmine: 'lib/jasmine/lib/jasmine-core/jasmine',
+    'jasmine-html': 'lib/jasmine/lib/jasmine-core/jasmine-html',
+    'jasmine-jquery': 'lib/jasmine-jquery/lib/jasmine-jquery'
+  },
+  packages: [{
+     name: "streamhub-sdk",
+     location: "src/"
+  },{
+     name: "streamhub-sdk-tests",
+     location: "tests/"
+  }],
+  shim: {
+    jquery: {
+        exports: '$'
+    },
+    jasmine: {
+        exports: 'jasmine'
+    },
+    'jasmine-html': {
+        deps: ['jasmine'],
+        exports: 'jasmine'
+    },
+    'jasmine-jquery': {
+        deps: ['jquery', 'jasmine']
+    }
+  }
+});
